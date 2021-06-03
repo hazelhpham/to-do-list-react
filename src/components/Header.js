7a169b488840335c9cb60132bb1edf8e283ffb5c
Hazel H Pham <51035837+hazelhpham@users.noreply.github.com>
2021-06-03 09:21:53 +0700

    backend json server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,21 @@
 //syntax: rafce
 import PropTypes from "prop-types";
 import Button from "./Button";
-const Header = ({ title }) => {
-  const onClick = (e) => {
-    console.log(e);
-  };
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1> {title} </h1>
-      <Button color="green" text="add" onClick={onClick} />
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add" }
+
+        onClick={onAdd}
+      />
     </header>
   );
 };
 
-//CSS in JS
-// const headingStyle = {
-//     color:'red',
-//     backgroundColor: 'black',
-// }
+
 
 //Catch error, restrict the type of props
 Header.propTypes = {
@@ -26,5 +24,3 @@ Header.propTypes = {
 export default Header;
 
 
-//o ben kia parameter la <Header title="hello"/>
-//in order to use the parameter: {props.title}
